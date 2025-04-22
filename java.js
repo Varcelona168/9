0e0be8619289f5243424b38e3c9730bb411119ab
Varcelona168 <nabdusalomova19@gmail.com>
2025-04-23 01:18:24 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+let body = document.getElementById(`body`)
+
+function bodyBGColor(rang) {
+
+
+    body.style.backgroundColor = rang
+}
+
+
+let h1 = document.getElementById(`text`)
+
+function color(rng) {
+
+    h1.style.color = rng
+    
+}
